ba5d0e7ca260aefcc3379135e46b7f3d6f5f6d37
FTM DEVELOPER <106005123+ftmdeveloperz@users.noreply.github.com>
2025-05-10 13:42:56 +0530

    Update config.py

--- api/config.py
+++ api/config.py
@@ -1,14 +1,19 @@
 # Replace with your working cookies
 cookies = {
-    'PANWEB': '1',
-    'browserid': 'p4nVrnlkUVKcnbbJHnIClAhSL5uXs01e-0svx0bm7KHLUB6wIVvCUNGLIpU=',
-    'lang': 'en',
-    '__bid_n': '1900b9f02442253dfe4207',
-    'ndut_fmt': 'BE5EF02E4FBDA93F542338752E051A84DEF30C5E3CBBF98408453BFE5D65FFE4',
-    '__stripe_mid': 'b85d61d2-4812-4eeb-8e41-b1efb3fa2a002a54d5',
-    'csrfToken': 'xknOoriwpXbwXMVswJ7kv1M7',
-    '__stripe_sid': 'e8fd1495-017f-4f05-949c-7cb3a1c780fed92613',
-    'ndus': 'YylKpiCteHuiYEqq8n75Tb-JhCqmg0g4YMH03MYD',
+    "PANWEB": "1",
+    "_ga_K6JMPYL99R": "GS2.1.s1746852518$o1$g1$t1746856379$j0$l0$h0",
+    "lang": "en",
+    "ab_ymg_result": '{"data":"da8926eb8716ff12c51c9bf40086f24127c2e3126b3a8c66530a42d50daa40797711e2873fe863d994f415fc0335acaddfb3aaeae3cc0a05231c5ffff2168e9b6f0aadbd833774602694a2e0fb899e27ca7e5aa108da809a42d639020c86fc4022ab1219e235d6c8b6819e3121cdb68975dec4ba636c79cbf05dcd8277382169","key_id":"66","sign":"e727e169"}',
+    "_ga_06ZNKL8C2E": "GS2.1.s1746858983$o2$g1$t1746858997$j46$l0$h0",
+    "__stripe_mid": "31fc92f3-a12a-480f-9e44-53f30f08258a75588e",
+    "_gcl_au": "1.1.432005496.1746852519",
+    "ndus": "YdZTyX1peHuimlux_D6dLGQBeHmj0r3M3trkunHB",
+    "_ga": "GA1.1.1786971687.1746852519",
+    "ab_sr": "1.0.1_N2I5MDI2MWFhMmMxOGE1MWU5ZTRlZjJhYzQwM2M0NmI4ZTZjNmZhZTQ5Nzk2MzAxNTA4ZmE3OTkyZTVhZGYyMzYyZTBlOWQ1NDUxMmE4YWQ3MWJhN2VhMzc4ZTI5YmFjZDA0ODBmNTRiMDU4MzRmOWZiYjc0ZjRiMGNlYzRmYTQ4MTYzZGI4Mzc5YzdhZDUyYWFkZjIxZTRiMTBkZTk5Zg==",
+    "__bid_n": "196b885ed7339790814207",
+    "browserid": "45Cbkepkx0J0bqgQi1e1ubtbstmebahkYOYm3ZWuIktZFHaUuRjvdeeHz24=",
+    "csrfToken": "7FTX6x-Gx7LCGACU9p9UBu4k",
+    "ndut_fmt": "047487DFA18CAB094A0E3D7CCAD71D9E1BDBAD24EAA82089BD2B24A3691BC457"
 }
 
 headers = {
