dc1663f16cfc5c87fd6124ea2c34a9b0774dd2e0
FTM DEVELOPER <106005123+ftmdeveloperz@users.noreply.github.com>
2025-05-10 12:32:43 +0530

    Update logger.py

--- api/logger.py
+++ api/logger.py
@@ -1,30 +1,15 @@
 import logging
-import os
 
-# Check if the environment is Lambda or similar, or use a local temporary directory
-log_dir = '/logs'  # Writable location for Lambda, or choose another location for your environment
-log_file_path = os.path.join(log_dir, 'api.log')
-
-# Create a logger instance
-logger = logging.getLogger('app_logger')
+logger = logging.getLogger("app_logger")
 logger.setLevel(logging.DEBUG)
 
-# Create file handler to write logs to a file in the writable directory
-file_handler = logging.FileHandler(log_file_path, encoding='utf-8')
-file_handler.setLevel(logging.DEBUG)
-
-# Create a console handler to log to console as well
+# Console handler (prints to stdout)
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
+console_handler.setLevel(logging.DEBUG)
 
-# Set the log format
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
 console_handler.setFormatter(formatter)
 
-# Add handlers to the logger
-logger.addHandler(file_handler)
 logger.addHandler(console_handler)
 
-# Log something to test
-logger.info("Logging setup completed")
+logger.info("Logger initialized successfully.")
